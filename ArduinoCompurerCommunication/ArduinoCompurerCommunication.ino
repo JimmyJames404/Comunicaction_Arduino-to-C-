2f6a9495ff4a2dfc6b8c861236381ec3a30858d5
Jimmy James <64377109+JimmyJames404@users.noreply.github.com>
2021-10-08 21:22:31 -0500

    Add files via upload

---
+++ ArduinoCompurerCommunication/ArduinoCompurerCommunication.ino
@@ -0,0 +1,172 @@
+#include <MatrizLed.h>
+#include <LiquidCrystal.h>
+#include <Servo.h>
+
+MatrizLed pantalla;
+Servo myservo;
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+String commandString = "";
+
+int led1Pin = 19;
+int led2Pin = 20;
+int led3Pin = 21;
+
+boolean isConnected = false;
+
+
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  
+  Serial.begin(9600);
+  pantalla.begin(16, 17, 18, 1); // dataPin, clkPin, csPin, numero de matrices de 8x8
+  myservo.attach(9);
+  pinMode(led1Pin,OUTPUT);
+  pinMode(led2Pin,OUTPUT);
+  pinMode(led3Pin,OUTPUT);
+  initDisplay();
+}
+
+void loop() {
+  
+ 
+
+  
+if(stringComplete)
+{
+  stringComplete = false;
+  getCommand();
+  
+  if(commandString.equals("STAR"))
+  {
+    lcd.clear();
+  }
+  if(commandString.equals("STOP"))
+  {
+    turnLedOff(led1Pin);
+    turnLedOff(led2Pin);
+    turnLedOff(led3Pin);
+    lcd.clear();
+    lcd.print("Ready to connect");    
+  }
+  else if(commandString.equals("TEXT"))
+  {
+    String text = getTextToPrint();
+    printText(text);
+    pantalla.borrar();
+    pantalla.escribirFraseScroll("jimmy", 200);
+  }
+  
+  else if(commandString.equals("LED1"))
+  {
+    boolean LedState = getLedState();
+    if(LedState == true)
+    {
+      turnLedOn(led1Pin);
+    }else
+    {
+      turnLedOff(led1Pin);
+    }   
+  }
+    else if(commandString.equals("LED2"))
+  {
+    boolean LedState = getLedState();
+    if(LedState == true)
+    {
+      turnLedOn(led2Pin);
+    }else
+    {
+      turnLedOff(led2Pin);
+    }   
+  }
+    else if(commandString.equals("LED3"))
+  {
+    boolean LedState = getLedState();
+    if(LedState == true)
+    {
+      turnLedOn(led3Pin);
+    }else
+    {
+      turnLedOff(led3Pin);
+    }   
+  }
+  
+  inputString = "";
+}
+
+}
+
+void initDisplay()
+{
+  lcd.begin(16, 2);
+  lcd.print("Ingrese texto");
+}
+
+boolean getLedState()
+{
+  boolean state = false;
+  if(inputString.substring(5,7).equals("ON"))
+  {
+    state = true;
+  }else
+  {
+    state = false;
+  }
+  return state;
+}
+
+void getCommand()
+{
+  if(inputString.length()>0)
+  {
+     commandString = inputString.substring(1,5);
+  }
+}
+
+void turnLedOn(int pin)
+{
+  digitalWrite(pin,HIGH);
+}
+
+void turnLedOff(int pin)
+{
+  digitalWrite(pin,LOW);
+}
+
+
+String getTextToPrint()
+{
+  String value = inputString.substring(5,inputString.length()-2);
+  return value;
+}
+
+void printText(String text)
+{
+  lcd.clear();
+  lcd.setCursor(0,0);
+    if(text.length()<16)
+    {
+      lcd.print(text);
+    }else
+    {
+      lcd.print(text.substring(0,16));
+      lcd.setCursor(0,1);
+      lcd.print(text.substring(16,32));
+    }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
